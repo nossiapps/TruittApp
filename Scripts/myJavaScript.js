5fef9f3a4c4fb3abdb92b446de302d3f853d6de9
nossiapps <nossiapps@gmail.com>
2015-06-12 09:03:15 -0500

    First Upload

---
+++ Scripts/myJavaScript.js
@@ -0,0 +1,9 @@
+var button1 = document.getElementById("container-1");
+var audioClip1 = document.getElementById("sound2");
+
+
+function play(){
+       var audioClip1 = document.getElementById("sound2");
+       audio.play();
+}
+
